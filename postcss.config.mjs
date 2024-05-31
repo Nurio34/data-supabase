2bb6f91852f095b274d588934c1579c3c8a5998d
Nurio34 <nurioonsoftware@gmail.com>
2024-05-31 04:34:01 +0300

    bugFix_cssNano

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,25 +1,8 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//   },
-// };
-
-// export default config;
-
-import cssnano from "cssnano";
-import postcssLoadConfig from "postcss-load-config";
-
-const config = async () => {
-    const { plugins } = await postcssLoadConfig();
-    return {
-        plugins: [
-            "postcss-preset-env",
-            // Conditionally add cssnano as a plugin
-            ...(process.env.NODE_ENV === "production" ? [cssnano()] : []),
-            ...plugins,
-        ],
-    };
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: {
+        tailwindcss: {},
+    },
 };
 
 export default config;
